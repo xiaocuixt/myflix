bde4695c030315cdfe85b29b7ac2ec35ac8132bb
xiaocuixt <cjw624923@126.com>
2016-06-10 09:21:21 +0800

    add shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 
 describe Category do
-	it "saves itself" do
-		category = Category.new(name: "comedies")
-		category.save
-		expect(Category.first).to eq(category)
-	end
+	# it "saves itself" do
+	# 	category = Category.new(name: "comedies")
+	# 	category.save
+	# 	expect(Category.first).to eq(category)
+	# end
 
-	it "has many videos" do
-		comedies = Category.create(name: "comedies")
-		south_park = Video.create(title: "South Park", description: "Funny video!", category: comedies)
-		futurama = Video.create(title: "Futurama", description: "Space Trval!", category: comedies)
-		expect(comedies.videos).to eq([futurama, south_park])
-	end
+	# it "has many videos" do
+	# 	comedies = Category.create(name: "comedies")
+	# 	south_park = Video.create(title: "South Park", description: "Funny video!", category: comedies)
+	# 	futurama = Video.create(title: "Futurama", description: "Space Trval!", category: comedies)
+	# 	expect(comedies.videos).to eq([futurama, south_park])
+	# end
+
+	# 上面的写法通过shoulda-matchers可以简化为：
+	it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,25 +1,30 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-  	video = Video.new(title: "monk", description: "a greate video")
-  	video.save
-  	expect(Video.first).to eq(video)
-  end
+  # it "saves itself" do
+  # 	video = Video.new(title: "monk", description: "a greate video")
+  # 	video.save
+  # 	expect(Video.first).to eq(video)
+  # end
 
-  it "belongs to category" do
-  	dramas = Category.new(name: "dramas")
-  	monk = Video.create(title: "monk", description: "a great video", category: dramas)
-    expect(monk.category).to eq(dramas)
-  end
+  # it "belongs to category" do
+  # 	dramas = Category.new(name: "dramas")
+  # 	monk = Video.create(title: "monk", description: "a great video", category: dramas)
+  #   expect(monk.category).to eq(dramas)
+  # end
 
-  it "does not save a video without a title" do
-  	video = Video.create(description: "this is a greate video")
-  	expect(Video.count).to eq(0)
-  end
+  # it "does not save a video without a title" do
+  # 	video = Video.create(description: "this is a greate video")
+  # 	expect(Video.count).to eq(0)
+  # end
 
-  it "does not save a video without a description" do
-  	video = Video.create(title: "monk")
-  	expect(Video.count).to eq(0)
-  end
+  # it "does not save a video without a description" do
+  # 	video = Video.create(title: "monk")
+  # 	expect(Video.count).to eq(0)
+  # end
+
+  # 上面的写法通过shoulda-matchers可以简化为：
+  it {should belong_to(:category)}
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:description)}
 end
