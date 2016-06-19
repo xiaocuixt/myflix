8bfcb32c2456b39e58db55b1b06b1865671c9277
xiaocuixt <cjw624923@126.com>
2016-06-19 13:59:38 +0800

    uncomment codes

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -35,29 +35,29 @@ describe ReviewsController do
           post :create, review: {rating: 4}, video_id: video.id
           expect(Review.count).to eq(0)
         end
-        # it "renders the videos/show template" do
-        #   video = Fabricate(:video)
-        #   post :create, review: {rating: 5}, video_id: video.id
-        #   expect(response).to render_template "videos/show"
-        # end
-        # it "sets @video" do
-        #   video = Fabricate(:video)
-        #   post :create, review: {rating: 5}, video_id: video.id
-        #   expect(assigns(:video)).to eq(video)
-        # end
-        # it "sets @reviews" do
-        #   review = Fabricate(:review, video: video)
-        #   post :create, review: {rating: 5}, video_id: video.id
-        #   expect(assigns(:reviews)).to match_array([review])
-        # end
+        it "renders the videos/show template" do
+          video = Fabricate(:video)
+          post :create, review: {rating: 5}, video_id: video.id
+          expect(response).to render_template "videos/show"
+        end
+        it "sets @video" do
+          video = Fabricate(:video)
+          post :create, review: {rating: 5}, video_id: video.id
+          expect(assigns(:video)).to eq(video)
+        end
+        it "sets @reviews" do
+          review = Fabricate(:review, video: video)
+          post :create, review: {rating: 5}, video_id: video.id
+          expect(assigns(:reviews)).to match_array([review])
+        end
       end
     end
-    # context "with unauthenticated users" do
-    #   it "redirects to the sign in path" do
-    #     video = Fabricate(:video)
-    #     post :create, review: {rating: 5}, video_id: video.id
-    #     expect(response).to redirect_to sign_in_path
-    #   end
-    # end
+    context "with unauthenticated users" do
+      it "redirects to the sign in path" do
+        video = Fabricate(:video)
+        post :create, review: {rating: 5}, video_id: video.id
+        expect(response).to redirect_to sign_in_path
+      end
+    end
   end
 end
