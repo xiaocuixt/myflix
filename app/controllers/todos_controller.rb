30505009b8285e9f61e4e97e0d2c911646fffd0a
xiaocuixt <cjw624923@126.com>
2016-06-25 21:46:13 +0800

    extract code from controller to model of todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -11,14 +11,7 @@ class TodosController < ApplicationController
 
   def create
   	@todo = Todo.new(todo_params)
-  	if @todo.save
-      location_string = @todo.name.slice(/.*\bAT\b(.*)/, 1).try(:strip)
-      if location_string
-        locations = location_string.split(/\,|and/).map(&:strip)
-        locations.each do |location|
-          @todo.tags.create(name: "location:#{location}")
-        end
-      end
+  	if @todo.save_with_tags
   		redirect_to root_path
   	else
   		render :new
