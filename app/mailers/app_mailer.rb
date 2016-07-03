f07cc8a3b2d6a3d55e4aa9e29f90246af44b6b07
xiaocuixt <cjw624923@126.com>
2016-07-03 11:44:51 +0800

    mailer configure & letter_opener

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,6 +1,6 @@
 class AppMailer < ActionMailer::Base
   def notify_on_new_to(user, todo)
     @todo = todo
-    mail from: "info@todoapp.com", to: user.email, subject: "You create a new todo #{todo}"
+    mail from: "info@todoapp.com", to: user.email, subject: "You create a new todo"
   end
 end
