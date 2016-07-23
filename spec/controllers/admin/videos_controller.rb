c4a359ffaaada9a311f0f299f4c6dd8ae770c8d1
xiaocuixt <cjw624923@126.com>
2016-07-23 14:50:22 +0800

    video new spec

--- spec/controllers/admin/videos_controller.rb
+++ spec/controllers/admin/videos_controller.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe Admin::VideosController do
   describe "GET new" do
     it_behaves_like "require_sign_in" do
-      let(:action){get :new}
+     let( :action ){ get :new }
     end
     it "sets the @video to a new video" do
-      set_current_user
+      set_current_admin
       get :new
       expect(assigns(:video)).to be_instance_of Video
       expect(assigns(:video)).to be_new_record
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -17,7 +17,7 @@ def set_current_user(user=nil)
   session[:user_id] = ( user || Fabricate(:user) ).id
 end
 
-def set_current_user(admin=nil)
+def set_current_admin(admin=nil)
   session[:user_id] = ( admin || Fabricate(:admin) ).id
 end
 
