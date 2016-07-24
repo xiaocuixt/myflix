7fcad32505acca55ba11788b6b6b80f217e2dbd2
xiaocuixt <cjw624923@126.com>
2016-07-24 12:51:30 +0800

    pay with stripe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    binding.pry
     if @user.save
       Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
       Stripe::Charge.create({
@@ -14,8 +13,6 @@ class UsersController < ApplicationController
         :currency => "usd",
         :source => params[:stripeToken],
         :description => "Sign UP Charge for #{@user.email}"
-      }, {
-        :idempotency_key => "yT9eixS61bFUSnvQ"
       })
       AppMailer.send_welcome_email(@user).deliver
       redirect_to sign_in_path
